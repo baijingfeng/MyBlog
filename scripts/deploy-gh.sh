62c386e7e643ffb44e076341f31dbd9e719aaa1d
baijingfeng <2596239285@qq.com>
2020-07-24 20:53:03 +0800

    update

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-yarn docs:build
+# yarn docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f https://github.com/baijingfeng/Myblog.git master:gh-pages
+git push -f https://github.com/baijingfeng/bookmarks.git master:gh-pages
 
 cd -
