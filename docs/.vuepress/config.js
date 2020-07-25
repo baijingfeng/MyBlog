45025287f724570dcd1149317b5a9c49c75cacd1
baijingfeng <2596239285@qq.com>
2020-07-25 12:03:45 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,71 @@
+const utils = require('./utils')
+
 module.exports = {
-  title: "My Blog",
-  description: "",
-  base: '/bookmarks/' 
+  title: "Bai Bookmarks",
+  description: "个人收藏夹",
+  base: '/bookmarks/',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico'
+      }
+    ]
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      // {
+      //   text: 'API',
+      //   link: '/apiWebsite/'
+      // },
+      {
+        text: 'Library',
+        link: '/library/'
+      },
+      {
+        text: '网站',
+        link: '/website/'
+      },
+      // {
+      //   text: '最近阅读',
+      //   link: '/diary/read'
+      // },
+      {
+        text: '文章',
+        link: '/article/'
+      },
+      // { text: '工具', link: 'https://google.com' },
+      // {
+      //   text: '面试',
+      //   link: '/interview/'
+      // },
+      // {
+      //   text: '内推',
+      //   link: '/job/'
+      // },
+      // {
+      //   text: 'Blog',
+      //   link: '/blog/'
+      // }
+    ],
+    sidebar: utils.inferSiderbars(),
+    lastUpdated: '上次更新',
+    repo: 'baijingfeng/bookmarks',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    sidebarDepth: 3
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@public': './public'
+      }
+    }
+  },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,8 @@
-# Hello VuePress!
+---
+home: true
+heroImage: /home.jpg
+actionText: 开始了解 →
+actionLink: /library/
+
+footer: MIT Licensed | Copyright © 2020-BaiJingfeng
+---
